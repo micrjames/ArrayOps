03256fba58c984d8fc7927f8a4d031d31f9e4ecf
Michael James <micrjames921@gmail.com>
2024-08-08 17:18:08 -0700

    wrote methods and tests

--- ArrayOps.ts
+++ ArrayOps.ts
@@ -2,18 +2,27 @@ export class ArrayOps<T> {
    constructor(private A: Array<T>, private B: Array<T>) {}
 
    intersection(): T[] {
-	  return Array<T>();
+	  return this.A.filter(x => this.B.includes(x));
    }
 
    difference(): T[] {
-	  return Array<T>();
+	  return this.A.filter(x => !this.B.includes(x));
    } 
 
    symmDifference(): T[] {
+	  // A Δ B = (A \ B) ∪ (B \ A)
 	  return Array<T>();
    }
 
    union(): T[] {
-	  return Array<T>();
+	  const mergedArray = [...this.A, ...this.B]; 
+	  const reducedArray = mergedArray.concat();
+
+	  for(let right = 0; right < reducedArray.length; right++)
+	  	 for(let left = 0; left < right; left++)
+		 	if(reducedArray[left] === reducedArray[right])
+			    reducedArray.splice(right, 1);
+
+	  return reducedArray.sort();
    }
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -3,15 +3,28 @@ import { ArrayOps } from "../ArrayOps";
 describe("Operations", () => {
    let A: number[] = [];
    let B: number[] = [];
-   let ops: ArrayOps<number> = new ArrayOps<number>(A, B);
+   let ops: ArrayOps<number>;
    describe("On Arrays", () => {
 	  beforeAll(() => {
 		 A = [1, 3, 4, 5];
 		 B = [1, 2, 5, 6, 7];
+		 ops = new ArrayOps<number>(A, B);
 	  });
-	  test.todo("Should intersect the arrays.");
-	  test.todo("Should give difference of the arrays.");
-	  test.todo("Should union the arrays.");
-	  test.todo("Should give the symmetric difference of the arrays.");
+	  test("Should intersect the arrays.", () => {
+		 const opsInt = ops.intersection(); // A ⋂ B
+		 expect(opsInt).toEqual([1, 5]);
+	  });
+	  test("Should give difference of the arrays.", () => {
+		 const opsDiff = ops.difference(); // A \ B
+		 expect(opsDiff).toEqual([3, 4]);
+	  });
+	  test("Should union the arrays.", () => {
+		 const opsUnion = ops.union(); // A ∪ B
+		 expect(opsUnion).toEqual([1, 2, 3, 4, 5, 6, 7]);
+	  });
+	  test("Should give the symmetric difference of the arrays.", () => {
+		 const opsSymmDifference = ops.symmDifference(); // A Δ B = (A\B) ∪ (B\A)
+		 expect(opsSymmDifference).toEqual([2, 3, 4, 6, 7]);
+	  });
    });
 });
